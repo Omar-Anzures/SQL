122f8d2dd1745542a1a76eb8c6e4ac28049c04e3
Omar-Anzures <omarsatiiago@gmail.com>
2025-04-25 17:36:30 -0600

    add excercives expedia-air

--- RetosSQL/question_dropbox.sql
+++ RetosSQL/question_dropbox.sql
@@ -31,7 +31,7 @@ INSERT INTO db_dept (id, department) VALUES(1, 'engineering'),
 
 
 SELECT ABS(
-    MAX(CASE WHEN d.department = 'marketing' THEN  e.salary END) -
+    MAX(CASE WHEN d.department = 'marketing' THEN  e.salary END)-
     MAX(CASE WHEN d.department = 'engineering' THEN  e.salary END)
     )difference
 FROM db_employee e JOIN db_dept d 
